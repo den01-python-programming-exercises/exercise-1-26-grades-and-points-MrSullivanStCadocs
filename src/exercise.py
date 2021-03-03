afb0fbb04e46eccb018e56c64dd996ff7362799f
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 11:15:14 +0000

    points variable and a lot of if and elif statements with less than or equal to.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #write your code below this line
+    points = int(input("Give points [0-100]:"))
+
+    if(points<0):
+      print("impossible")
+    elif(points<=49):
+      print("failed")
+    elif(points<=59):
+      print("1")
+    elif(points<=69):
+      print("2")
+    elif(points<=79):
+      print("3")
+    elif(points<=89):
+      print("4")
+    elif(points<=100):
+      print("5")
+    elif(points>100):
+      print("incredible")
 
 if __name__ == '__main__':
     main()
